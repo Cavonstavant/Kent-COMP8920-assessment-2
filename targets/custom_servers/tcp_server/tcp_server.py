d20428c5389b8d3b5fd9ca6e3b980b32a76b67c5
Cavonstavant <38893947+Cavonstavant@users.noreply.github.com>
2024-03-19 14:48:53 +0000

    targets(tcp_server): Add basic TCP server

---
+++ targets/custom_servers/tcp_server/tcp_server.py
@@ -0,0 +1,18 @@
+import socketserver
+import sys
+
+class ExampleTCPHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        self.data = self.request.recv(1024).strip()
+        print("{} wrote:".format(self.client_address[0]))
+        print(self.data)
+        if b'secret' in self.data:
+            self.request.sendall(b"TCP secret: 5678\n")
+        else:
+            self.request.sendall(b"Basic TCP server\n")
+
+if __name__ == "__main__":
+    HOST, PORT = sys.argv[1] or "0.0.0.0", int(sys.argv[2]) or 9999
+    with socketserver.TCPServer((HOST, PORT), ExampleTCPHandler) as server:
+        server.allow_reuse_address = True
+        server.serve_forever()
